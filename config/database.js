71d7d36c6b7de4dcca0c9260223fdfe6f78129fd
BobElegante <andresmanuelh77@gmail.com>
2025-06-07 13:29:20 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 // src/config/database.js
 // require('dotenv').config(); // Ya se carga en app.js o se pasan por Docker Compose. Puedes comentarlo si no lo necesitas aquí.
 
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-// Imprime las variables de entorno para depurar (opcional, considera eliminar en prod)
-console.log('Variables de entorno en database.js:', {
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_NAME: process.env.DB_NAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_PORT: process.env.DB_PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
-});
+// // Imprime las variables de entorno para depurar (opcional, considera eliminar en prod)
+// console.log('Variables de entorno en database.js:', {
+//   DB_USER: process.env.DB_USER,
+//   DB_HOST: process.env.DB_HOST,
+//   DB_NAME: process.env.DB_NAME,
+//   DB_PASSWORD: process.env.DB_PASSWORD,
+//   DB_PORT: process.env.DB_PORT,
+//   DATABASE_URL: process.env.DATABASE_URL,
+// });
 
 // Crear el pool de conexiones
 // const pool = new Pool({
@@ -25,21 +25,28 @@ console.log('Variables de entorno en database.js:', {
 //   port: process.env.DB_PORT, // Se espera que sea un número, dotenv lo leerá como string. Asegúrate que es '5432'
 //   url: process.env.DATABASE_URL,
 // });
+const { Pool } = require('pg');
+
+console.log('Conectando con:', process.env.DATABASE_URL);
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
+  // Fuerza IPv4 (opcional)
   family: 4
 });
 
-
-// Probar la conexión
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Error al conectar con la base de datos:', err.stack);
-  } else {
-    console.log('Conexión exitosa a la base de datos');
-    release(); // Liberar el cliente después de probar la conexión
-  }
-});
-
 module.exports = pool;
+
+
+// // Probar la conexión
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     console.error('Error al conectar con la base de datos:', err.stack);
+//   } else {
+//     console.log('Conexión exitosa a la base de datos');
+//     release(); // Liberar el cliente después de probar la conexión
+//   }
+// });
+
+// module.exports = pool;
